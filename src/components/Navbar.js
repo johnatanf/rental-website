32eb0c59bd68007ae817509cb8bf4263de001336
johnatanf <johnatan.f100@gmail.com>
2021-04-20 15:10:17 +0700

    add Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import * as React from "react"
+import "../css/style.css"
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <p className="navbar__logo">logo</p>
+        <ul className="navbar__group">
+            <li className="navbar__item">
+                <a href="#">
+                    Home
+                </a>
+            </li>
+            <li className="navbar__item">
+                <a href="#">
+                    Rooms
+                </a>
+            </li>
+            <li className="navbar__item">
+                <a href="#">
+                    Check availability
+                </a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react"
 import "../css/style.css"
 import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
 
 const IndexPage = () => {
   return (
     <>
+      <Navbar />
       <main>
 
       </main>
