a723d5163da5ce7dde1b65f70a88d6ecfc983fe0
johnatanf <johnatan.f100@gmail.com>
2021-04-20 16:17:42 +0700

    add social links to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,29 @@
 import * as React from "react"
 import "../css/style.css"
+import iconSet from "../images/sprite.json"
+import IcomoonReact, { iconList } from "icomoon-react"
+
+console.log('icons: ' + iconList(iconSet))
 
 const Header = () => {
+    
     return (
-        <header class="header">
-            <div class="header__container">
-                <h1 class="header__heading">Hotel Name</h1>
+        <header className="header">
+            <div className="header__container">
+                <h1 className="header__heading">Hotel Name</h1>
                 <hr />
-                <p class="header__description">Insert description here</p>
+                <p className="header__description">Insert description here</p>
+                <div className="header__socials">
+                    <a href="#">
+                        <IcomoonReact className="header__socials-icon" iconSet={iconSet} icon="twitter" />
+                    </a>
+                    <a href="#">
+                        <IcomoonReact className="header__socials-icon" iconSet={iconSet} icon="facebook2" />
+                    </a>
+                    <a href="#">
+                        <IcomoonReact className="header__socials-icon" iconSet={iconSet} icon="instagram" />
+                    </a>
+                </div>
             </div>
         </header>
     )
