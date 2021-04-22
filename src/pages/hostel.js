4ab7c09eecffddfbacc8d75118378e8875b28802
Johnatan Fallen <johnatan.f100@gmail.com>
2021-04-22 16:54:02 +0700

    create template for room pages, create hostel page, make slight changes to card styles

--- src/pages/hostel.js
+++ src/pages/hostel.js
@@ -4,8 +4,10 @@ import { graphql } from "gatsby"
 import "../css/style.css"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
+import Room from "../templates/room"
+import hostelImage from "../images/hostel.jpg"
 
-const BookReservationPage = ({ data }) => {
+const Hostel = ({ data }) => {
   
     return (
     <>
@@ -15,14 +17,21 @@ const BookReservationPage = ({ data }) => {
       </Helmet>
       <Navbar />
       <main className="main">
-        
+        <Room 
+          type="Hostel"
+          price="15"
+          image={hostelImage}
+        >
+          <p className="paragraph">Aliquam rhoncus dui sed nisl dictum, id euismod tellus tristique. Nulla ut lacus nec neque feugiat pulvinar. Vivamus sed metus in velit efficitur faucibus et convallis libero. Suspendisse efficitur nulla non feugiat vestibulum. In quis tristique leo. Curabitur et condimentum elit, ac varius lorem. Ut condimentum, mi eu dapibus interdum, augue est pellentesque lorem, vitae egestas neque ligula ac sapien.</p>
+          <p className="paragraph">Ut rutrum tincidunt eros quis rutrum. Sed consectetur, justo quis suscipit pulvinar, nisl mi tempus orci, et consectetur magna sem ac nisi. Cras ultricies condimentum tortor, ut porttitor eros congue at.</p>
+        </Room>
       </main>
       <Footer />
     </>
   )
 }
 
-export default BookReservationPage
+export default Hostel
 
 export const query = graphql`
   query {
--- src/templates/room.js
+++ src/templates/room.js
@@ -1,10 +1,61 @@
 import * as React from "react"
+import { Link } from "gatsby"
 import "../css/style.css"
+import hostelImage from "../images/hostel.jpg"
+import Card from "../components/Cards/Card"
+import CardContainer from "../components/Cards/CardContainer"
 
-const Room = () => {
+const Room = props => {
   return (
     <>
-      
+      <section className="section section--dark section-room-description">
+        <h1 className="room__heading">{props.type}</h1>
+        <div className="room__content-container">
+          <img className="room__image" src={props.image} />
+          <div className="room__paragraph-container">
+            {props.children}
+            <p className="room__price">{`Starting from $${props.price}/night`}</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="section section--dark section-room-facilities">
+        <h2 className="heading-secondary">Facilities</h2>
+        <CardContainer>
+          <Card 
+            icon="snowflake-o"
+            heading="air conditioning"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mi."
+          />
+          <Card 
+            icon="restaurant"
+            heading="free breakfast"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam venenatis."
+          />
+          <Card 
+            icon="automobile"
+            heading="free parking"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at."
+          />
+          <Card 
+            icon="wifi"
+            heading="WiFi"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nisi."
+          />
+          <Card 
+            icon="fingerprint"
+            heading="fingerprint access"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse convallis."
+          />
+          <Card 
+            icon="display"
+            heading="television"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mattis."
+          />
+        </CardContainer>
+
+        <Link className="room__book-button" to={'/book-reservation'}>Book now</Link>
+      </section>
     </>
   )
 }
